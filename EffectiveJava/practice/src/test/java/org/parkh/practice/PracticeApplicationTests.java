d0a053a4da30e021d375a421cab93040c2172e99
phj1120 <hyeonj1998@naver.com>
2023-04-17 00:52:02 +0900

    Effective Java

---
+++ EffectiveJava/practice/src/test/java/org/parkh/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.parkh.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
