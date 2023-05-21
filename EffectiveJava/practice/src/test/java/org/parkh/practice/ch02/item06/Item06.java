f29eca5ff6fe1a69fe59a46f7bbb95df7a76e1e5
phj1120 <hyeonj1998@naver.com>
2023-05-21 22:47:44 +0900

    아이템06. 불필요한 객체 생성을 피하라

--- EffectiveJava/practice/src/test/java/org/parkh/practice/ch02/item06/Item06.java
+++ EffectiveJava/practice/src/test/java/org/parkh/practice/ch02/item06/Item06.java
@@ -1,12 +1,17 @@
 package org.parkh.practice.ch02.item06;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
 import java.util.Set;
 
+@Slf4j
 public class Item06 {
 
+    /**
+     * keySet 같은 객체 반환함
+     */
     @Test
     void MapInterface() {
         HashMap<String, Integer> map = new HashMap<>();
@@ -20,6 +25,25 @@ public class Item06 {
 
         map.put("3", 3);
         Set<String> keySet4 = map.keySet();
+
+        log.info("End");
     }
 
+    @Test
+    void autoBoxing() {
+        Long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        log.info(String.valueOf(sum));
+    }
+
+    @Test
+    void autoBoxing2() {
+        long sum = 0L;
+        for (long i = 0; i < Integer.MAX_VALUE; i++) {
+            sum += i;
+        }
+        log.info(String.valueOf(sum));
+    }
 }
